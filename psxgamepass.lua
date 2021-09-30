a2dbd99820e5b9f2003ac664fbe7dc047413accb
mayisurvivee <91602664+mayisurvivee@users.noreply.github.com>
2021-09-30 13:00:22 +0700

    Create psxgamepass.lua

---
+++ psxgamepass.lua
@@ -0,0 +1,10 @@
+local main = debug.getupvalues(require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library")).Save.Get)[2][game.Players.LocalPlayer].save.Gamepasses
+table.insert(main,18674296)
+table.insert(main,18674298)
+table.insert(main,18674321)
+table.insert(main,21641016)
+table.insert(main,21583760)
+table.insert(main,18674307)
+table.insert(main,21176989)
+table.insert(main,18674305)
+table.insert(main,18829757)
